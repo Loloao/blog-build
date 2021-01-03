796e1ab33c10973ee39066729061788f38943600
li <l1312213036@outlook.com>
2021-01-03 17:42:31 +0800

    fix: ol dont show index

--- src/pages/detail/components/MarkdownReader/index.tsx
+++ src/pages/detail/components/MarkdownReader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useLayoutEffect} from 'react'
 import ReactMarkdown from 'react-markdown'
 import styles from './styles.module.scss'
 
@@ -8,6 +8,20 @@ interface Props {
 
 const MarkdownReader = (props: Props) => {
     const {text} = props
+    const getOlLiIndexElem = (index: number) => {
+        const wrapper = document.createElement('div')
+        wrapper.classList.add(styles['ol-li-index'])
+        wrapper.innerText = `${index + 1}.`
+        return wrapper
+    }
+    useLayoutEffect(() => {
+        // 用于解决 ol 标签不显示前缀的问题
+        document.getElementsByClassName(styles['reader'])[0].querySelectorAll('ol').forEach(v => {
+            v.querySelectorAll('li').forEach((v1, i) => {
+                v1.appendChild(getOlLiIndexElem(i))
+            })
+        })
+    })
     return <>
         <ReactMarkdown children={text} className={styles['reader']} />
     </>
