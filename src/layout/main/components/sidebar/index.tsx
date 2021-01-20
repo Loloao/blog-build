cc2624ba4c5d5531ea0ff90a649638dfd2146883
Loloao <l1312213036@outlook.com>
2021-01-20 20:59:40 +0800

    feat: add content sidebar

--- src/layout/main/components/sidebar/index.tsx
+++ src/layout/main/components/sidebar/index.tsx
@@ -6,7 +6,28 @@ interface Props {
 }
 
 const Sidebar = (props: Props) => {
-  return <div className={styles['content-sidebar']}>{props.children}</div>
+  return (
+    <div className={styles['content-sidebar']}>
+      <div className={styles['recent-update']}>
+        <h3>最近更新</h3>
+        <ul className={styles['update-list']}>
+          <li>kankanksndf</li>
+          <li>ksjflajsdklfjasd</li>
+          <li>lskdjfl;a;fas;</li>
+          <li>lasdfjl;ajsd</li>
+        </ul>
+      </div>
+      <div className={styles['trend-tags']}>
+        <h3>标签趋势</h3>
+        <ul className={styles['tags-list']}>
+          <li>kankan</li>
+          <li>ksjfla</li>
+          <li>lskdj</li>
+          <li>las</li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar
