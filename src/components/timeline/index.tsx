8fa375138303909d3592ba0b43427726f51b0377
liyang <l1312213036@outlook.com>
2021-02-06 19:15:01 +0800

    feat: complete timeline

--- src/components/timeline/index.tsx
+++ src/components/timeline/index.tsx
@@ -5,8 +5,12 @@ import classnames from 'classnames'
 
 export interface timelineItem {
   title?: string
+}
+
+export interface sublineItem {
   link?: string
   route?: string
+  time?: string
 }
 
 interface props {
@@ -14,45 +18,94 @@ interface props {
 }
 
 const Timeline = (props: props) => {
-  const getData = (item: timelineItem, index: number) => {
-    if (item.title) {
+  const formatTime = (time) => `${new Date(time).getMonth() + 1} 月 ${new Date(time).getUTCDate() + 1} 日`
+  const sortSublineByTime = (data) =>
+    data.sort((a, b) => {
+      const prev = new Date(a.time).getTime()
+      const next = new Date(b.time).getTime()
+      return next - prev
+    })
+  const transferDate = (times: string) => {
+    const years = new Map()
+    sortSublineByTime(times)
+    console.log(JSON.parse(JSON.stringify(times)), 'times')
+    return times.reduce<timelineItem | sublineItem[]>((sum, next, index) => {
+      const year = new Date(next.time).getFullYear()
+      if (!years.has(year)) {
+        years.set(year, sum.length)
+        sum.push({ title: year })
+      } else {
+        const currentYear = years.get(year)
+        const subline = sum[currentYear + 1]
+        if (Array.isArray(subline)) {
+          next.time = formatTime(next.time)
+          subline.push(next)
+          console.log(next, ' next')
+        } else {
+          next.time = formatTime(next.time)
+          sum[currentYear + 1] = [next]
+        }
+      }
+      return sum
+    }, [])
+  }
+
+  const getData = (item1: timelineItem | sublineItem[], index: number) => {
+    if (item1.title) {
       return (
-        <div className={classnames(styles['title-wrapper'], styles['hasBottomLine'])} key={item.title}>
-          <h3>{item.title}</h3>
+        <div
+          className={classnames({
+            [styles['title-wrapper']]: true,
+            [styles['hasTopLine']]: !(index === 0)
+          })}
+          key={item1.title}
+        >
+          <h3>{item1.title}</h3>
         </div>
       )
-    } else if (item.time && item.link) {
+    } else {
       return (
-        <div className={styles['link-wrapper']} key={item.link}>
-          <div className={classnames(styles['link-left'], styles['hasBottomLine'])}>{item.time}</div>
-          <Link path={item.route} className={styles['link-right']}>
-            <div>{item.link}</div>
-          </Link>
-        </div>
+        <ul className={styles['subline-wrapper']} key={index}>
+          {item1.map((item, index, arr) => {
+            return (
+              <li className={styles['link-wrapper']} key={item.link}>
+                <div
+                  className={classnames({
+                    [styles['link-left']]: true,
+                    [styles['hasTopLine']]: true
+                  })}
+                >
+                  {item.time}
+                </div>
+                <Link to={item.route} className={styles['link-right']}>
+                  {item.link}
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
       )
     }
   }
 
-  return (
-    <div className={styles['timeline-wrapper']}>
-      {props.data.map((v, i) => {
-        return getData(v, i)
-      })}
-    </div>
-  )
+  return <div className={styles['timeline-wrapper']}>{transferDate(props.data).map(getData)}</div>
 }
 
 const data = [
   { title: '2019' },
-  { time: '2020.1.1', link: '1231231', route: 'www.baidu.com' },
-  { time: '2020.1.1', link: '2131231', route: 'www.baidu.com' },
-  { time: '2020.1.1', link: '12312312', route: 'www.baidu.com' },
-  { time: '2020.1.1', link: '1231213', route: 'www.baidu.com' },
+  [
+    { time: '2020.1.1', link: '1231231', route: 'www.baidu.com' },
+    { time: '2020.1.1', link: '2131231', route: 'www.baidu.com' },
+    { time: '2020.1.1', link: '12312312', route: 'www.baidu.com' },
+    { time: '2020.1.1', link: '1231213', route: 'www.baidu.com' }
+  ],
   { title: '2020' },
-  { time: '2020.1.2', link: '1214124', route: 'www.baidu.com' },
-  { time: '2020.1.3', link: '123141', route: 'www.baidu.com' },
-  { time: '2020.1.4', link: '1231', route: 'www.baidu.com' },
-  { time: '2020.1.5', link: '15123', route: 'www.baidu.com' }
+  [
+    { time: '2020.1.2', link: '1214124', route: 'www.baidu.com' },
+    { time: '2020.1.3', link: '123141', route: 'www.baidu.com' },
+    { time: '2020.1.4', link: '1231', route: 'www.baidu.com' },
+    { time: '2020.1.5', link: '15123', route: 'www.baidu.com' }
+  ]
 ]
 
 Timeline.defaultProps = {
--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -1,20 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import styles from './styles.module.scss'
-import Timeline from '@/components/timeline'
+import Timeline, { sublineItem } from '@/components/timeline'
 import { getTimelineIssues } from '@/utils/request'
 import { IssueDetail } from '@/utils/classes'
+import { stringifySearch } from '@/utils'
 
 const Categories = () => {
-  const [issues, setIssues] = useState<IssueDetail[]>([])
+  const [times, setTimes] = useState<sublineItem[]>([])
 
   useEffect(() => {
-    getTimelineIssues().then(setIssues)
+    getTimelineIssues().then((res) => {
+      setTimes(
+        res.map<sublineItem>((v) => {
+          return {
+            time: v.created_at,
+            link: v.title,
+            route: { pathname: '/detail', search: `${stringifySearch({ issueNumber: v.number })}` }
+          }
+        })
+      )
+    })
   }, [])
 
   return (
     <div className={styles['categories-wrapper']}>
       <h2 className={styles['categories-title']}>时间线</h2>
-      <Timeline />
+      <Timeline data={times} />
     </div>
   )
 }
