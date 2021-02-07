be3f260a8bc777c25b1dd753aa0c4c5c64427398
liyang <l1312213036@outlook.com>
2021-02-07 21:01:10 +0800

    add about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import styles from './styles.module.scss'
 
 const About = () => {
-  return <div>about</div>
+  return (
+    <div className={styles['about-wrapper']}>
+      <h2 className={styles['about-title']}>关于</h2>
+    </div>
+  )
 }
 
 export default About
